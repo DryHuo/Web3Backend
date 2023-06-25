ba6e19d34a8059fb90d4905aed54df0a8f4a8677
nociza <azicon@berkeley.edu>
2023-06-26 00:24:43 +0800

    add scan and prepare for deployment test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,33 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-etherscan";
+import "@nomicfoundation/hardhat-ethers";
 
-if (!process.env.PRIVATE_KEY) {
-  throw new Error("Please set your PRIVATE_KEY in a .env file");
+if (!process.env.PRIVATE_KEY || !process.env.POLYGONSCAN_API_KEY) {
+  throw new Error(
+    "Please set your PRIVATE_KEY and POLYSCAN_API_KEY in a .env file"
+  );
 }
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  defaultNetwork: "polygon_mumbai",
   networks: {
-    mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com/",
+    hardhat: {},
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
       accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  },
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
     },
   },
 };
