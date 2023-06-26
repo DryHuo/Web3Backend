533e62f8df1cd45f303210f8d51e3f07458a2ffc
nociza <azicon@berkeley.edu>
2023-06-26 23:33:42 +0800

    minting logic done

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,32 @@
+import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getAddress()).toString());
+
+  // Deploy the WikiToken
+  const WikiTokenFactory = await ethers.getContractFactory("WikiToken");
+  const wikiToken = (await WikiTokenFactory.deploy(
+    ethers.parseEther("1000000")
+  )) as Contract; // Replace with desired initial supply
+  console.log("WikiToken address:", wikiToken.address);
+
+  // Deploy the DAO
+  const DAOFactory = await ethers.getContractFactory("DAO");
+  const dao = (await DAOFactory.deploy(wikiToken.address)) as Contract; // This assumes the DAO contract takes the WikiToken's address in the constructor
+  console.log("DAO address:", dao.address);
+
+  // Lock logic (replace with sdesired lock logic)
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
-
   const lockedAmount = ethers.parseEther("0.001");
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
+  const Lock = await ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
   console.log(
     `Lock with ${ethers.formatEther(
       lockedAmount
@@ -19,8 +34,6 @@ async function main() {
   );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
