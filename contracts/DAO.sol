d5473161a57b60f603576d1fbd862bf3aa48efa3
nociza <azicon@berkeley.edu>
2023-06-26 22:57:39 +0800

    add tax logic

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract DAO {
+    IERC20 public token;
+
     struct Proposal {
         string description;
         address proposer;
@@ -11,20 +15,46 @@ contract DAO {
     }
 
     address public owner;
+    address public taxAccount;
+    uint256 public minStake;
+    uint256 public constant TAX_RATE = 2; // The fee rate is 2%
+
     mapping(address => bool) public isBoardMember;
     mapping(address => bool) public isMember;
     uint256 public totalAssetPool;
     Proposal[] public proposals;
 
-    constructor(uint256 initialAssetPool) {
+    constructor(
+        address tokenAddress,
+        address _LifeWikiAssetPool,
+        uint256 _minStake,
+        uint256 initialAssetPool
+    ) {
+        require(
+            initialAssetPool >= _minStake,
+            "Initial asset pool must be at least minimum stake"
+        );
+        token = IERC20(tokenAddress);
         owner = msg.sender;
+        taxAccount = _LifeWikiAssetPool;
+        minStake = _minStake;
         totalAssetPool = initialAssetPool;
+        _transferTokens(msg.sender, address(this), initialAssetPool);
+        _transferTokens(
+            msg.sender,
+            taxAccount,
+            (initialAssetPool * TAX_RATE) / 100
+        );
     }
 
-    function joinAsBoardMember() external {
+    function joinAsBoardMember(uint256 stake) external {
+        require(stake >= minStake, "Stake must be at least minimum stake");
         require(!isMember[msg.sender], "Already a regular member");
         require(!isBoardMember[msg.sender], "Already a board member");
         isBoardMember[msg.sender] = true;
+        totalAssetPool += stake;
+        _transferTokens(msg.sender, address(this), stake);
+        _transferTokens(msg.sender, taxAccount, (stake * TAX_RATE) / 100);
     }
 
     function joinAsRegularMember() external {
@@ -56,5 +86,14 @@ contract DAO {
         if (accept) {
             proposals[proposalIndex].isAccepted = true;
         }
+    }
+
+    // Internal function to handle transferring tokens
+    function _transferTokens(
+        address from,
+        address to,
+        uint256 amount
+    ) internal {
+        require(token.transferFrom(from, to, amount), "Token transfer failed");
     }
 }
