e2215c0aca73bd805ae2555515e738234583f076
Steve Gao <steve_gao@berkeley.edu>
2023-06-27 02:12:31 -0700

    change token

--- contracts/WikiToken.sol
+++ contracts/WikiToken.sol
@@ -2,42 +2,20 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract WikiToken is ERC20, ERC20Burnable, ERC20Pausable, AccessControl {
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+contract WikiToken is ERC20, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor(uint256 initialSupply) ERC20("WikiToken", "WIKI") {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(PAUSER_ROLE, msg.sender);
         _setupRole(MINTER_ROLE, msg.sender);
 
         _mint(msg.sender, initialSupply);
     }
 
-    function pause() public {
-        require(hasRole(PAUSER_ROLE, msg.sender));
-        _pause();
-    }
-
-    function unpause() public {
-        require(hasRole(PAUSER_ROLE, msg.sender));
-        _unpause();
-    }
-
     function mint(address to, uint256 amount) public {
         require(hasRole(MINTER_ROLE, msg.sender));
         _mint(to, amount);
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override(ERC20, ERC20Pausable) whenNotPaused {
-        super._beforeTokenTransfer(from, to, amount);
     }
 }
